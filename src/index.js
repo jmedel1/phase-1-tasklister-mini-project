e7655f949ccadf73249e56506d9f617da635e786
jael <jael.medel30@gmail.com>
2023-04-13 21:29:10 -0500

    task list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(event){ 
+    event.preventDefault();
+    
+    const nameInput = document.getElementById("new-task-description");
+    const name = nameInput.value;
+    console.log("Form submitted with name:", name);
+
+    nameInput.value = "";
+  });
 });
